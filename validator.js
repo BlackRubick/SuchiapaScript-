48b7fbb31446794efbe0310706cc3e7e9bb4a94f
Cesar Gómez Aguilera <94030723+BlackRubick@users.noreply.github.com>
2023-11-05 22:19:08 -0600

    fisrt commit jijijija
    HOLA UWU

---
+++ validator.js
@@ -0,0 +1,89 @@
+function validateCode() {
+    const code = document.getElementById('codeInput').value.split('\n');
+
+    const tokens = {
+        'variableDeclaration': /(\w+)->\s*("[^"]+"|\d+)/,
+        'functionDeclaration': /act\s+(\w+)\s*\(.*\)\s*\{/,
+        'forLoop': /vos\s+\w+\s+en\s+\(\d+,\s*\d+\)\s*\{/,
+        'conditional': /ira\s+.+\s*\{/,
+        'elseStatement': /tons\s*\{/,
+        'returnStatement': /devuelto\s+.+/,
+        'printStatement': /mostralo\(.+\)/,
+        'closeBlock': /\}/,
+        'assignment': /\w+\s*=\s*.+/
+    };
+
+    
+    let openBlocks = 0;
+    let wasIra = false;
+    const declaredFunctions = new Set();
+
+    for (let i = 0; i < code.length; i++) {
+        let line = code[i].trim();
+
+        if (!line) continue;
+
+        let validLine = false;
+        for (let key in tokens) {
+            const match = line.match(tokens[key]);
+            if (match) {
+                validLine = true;
+
+                if (key === 'functionDeclaration') {
+                    const functionName = match[1];
+                    if (declaredFunctions.has(functionName)) {
+                        alert(`ey vos hija  tenes un Error en línea ${i + 1}: La función "${functionName}" ya ha sido declarada hija `);
+                        return;
+                    }
+                    declaredFunctions.add(functionName);
+
+                    openBlocks++;
+                } else if (key === 'forLoop' || key === 'conditional') {
+                    openBlocks++;
+                    if (key === 'conditional') wasIra = true;
+                } else if (key === 'elseStatement') {
+                    if (wasIra) {
+                        wasIra = false;
+                    } else {
+                        openBlocks++;
+                    }
+                } else if (key === 'closeBlock') {
+                    openBlocks--;
+                    if (openBlocks < 0) {
+                        alert(`ey vos hija  tenes un Error en línea${i + 1}: Bloque de cierre innecesario.`);
+                        return;
+                    }
+                }
+
+                break;
+            }
+        }
+
+        if (!validLine) {
+            alert(`ey vos hija  tenes un Error en línea ${i + 1}: "${line}"`);
+            return;
+        }
+    }
+
+    if (openBlocks !== 0) {
+        alert("ey vos hija  tenes un error : Falta cerrar uno o más bloques.");
+        return;
+    }
+
+    alert('simon hija Código válido');
+}
+document.getElementById('codeInput').oninput = function() {
+    rows();
+}
+
+const rows = () => {
+    const code = document.getElementById('codeInput').value.split('\n');
+    const rowContainer = document.getElementById('rowCode');
+    rowContainer.innerHTML = '';
+
+    code.forEach((line, index) => {
+        const p = document.createElement('p');
+        p.textContent = index + 1;
+        rowContainer.appendChild(p);
+    });
+}
